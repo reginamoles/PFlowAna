a7651b8742f74c8be54bb893a9f5aed413875d74
reginamoles <reginamoles@gmail.com>
2016-03-14 17:31:06 +0100

    path fixed

--- util/testRun.cxx
+++ util/testRun.cxx
@@ -20,18 +20,13 @@ int main( int argc, char* argv[] ) {
   xAOD::Init().ignore();
 
   // Construct the samples to run on:
-  SH::SampleHandler sh;
-
   // use SampleHandler to scan all of the subdirectories of a directory for particular MC single file:
-  //const char* inputFilePath = gSystem->ExpandPathName ("$ALRB_TutorialData/p2452/");
-  //SH::ScanDir().filePattern("DAOD_SUSY1.07053638._000013.pool.root.1").scan(sh,inputFilePath);
-
-  SH::DiskListLocal list("./../../../../../workspace/MC/AOD/JZ3/user.moles.147913.Pythia8_AU2CT10_jetjet_JZ3W.recon.AOD.e3099_s2832_r7617_AOD.67276\
-209/");
-  SH::scanFiles(sh, list);
-
-
-
+  SH::SampleHandler sh;
+  const char* inputFilePath = gSystem->ExpandPathName ("$DataFiles/JZ3/user.moles.147913.Pythia8_AU2CT10_jetjet_JZ3W.recon.AOD.e3099_s2832_r7617_AOD.67276209/");
+  SH::ScanDir().filePattern("user.moles.7760083.AOD._000049.pool.root").scan(sh,inputFilePath); //One indiviudual file
+  //SH::ScanDir().scan(sh,inputFilePath); //All files in the directory
+  
+  
   // Set the name of the input TTree. It's always "CollectionTree"
   // for xAOD files.
   sh.setMetaString( "nc_tree", "CollectionTree" );
@@ -42,7 +37,7 @@ int main( int argc, char* argv[] ) {
   // Create an EventLoop job:
   EL::Job job;
   job.sampleHandler( sh );
-  job.options()->setDouble (EL::Job::optMaxEvents, 500);
+  job.options()->setDouble (EL::Job::optMaxEvents, 10);
 
   // Add our analysis to the job:
   xAODPFlowAna* alg = new xAODPFlowAna();
