9512baa68f4f9e9ebf2b6a4340971ee1b6f442d5
reginamoles <reginamoles@gmail.com>
2016-08-24 16:59:57 +0200

    Config options included

--- util/testRun.cxx
+++ util/testRun.cxx
@@ -22,14 +22,21 @@ int main( int argc, char* argv[] ) {
   // Construct the samples to run on:
   // use SampleHandler to scan all of the subdirectories of a directory for particular MC single file:
   SH::SampleHandler sh;
+
   //DiJetSample
   //const char* inputFilePath = gSystem->ExpandPathName ("$DataFiles/JZ3/user.moles.147913.Pythia8_AU2CT10_jetjet_JZ3W.recon.AOD.e3099_s2832_r7617_AOD.67276209/");
   //SH::ScanDir().filePattern("user.moles.7760083.AOD._000102.pool.root").scan(sh,inputFilePath); //One indiviudual file
   //SH::ScanDir().scan(sh,inputFilePath); //All files in the directory
 
   //SinglePions
-  const char* inputFilePath = gSystem->ExpandPathName ("$DataFiles/SinglePions/mc15c_piplus/user.moles.mc15_13TeV.428001.ParticleGun_single_piplus_logE0p2to2000.recon.AOD.e3501_s2832_r8014_AOD/");
-  SH::ScanDir().filePattern("user.moles.8671065.AOD._000411.pool.root").scan(sh,inputFilePath); //One indiviudual file
+  //const char* inputFilePath = gSystem->ExpandPathName ("$DataFiles/SinglePions/mc15c_piplus/user.moles.mc15_13TeV.428001.ParticleGun_single_piplus_logE0p2to2000.recon.AOD.e3501_s2832_r8014_AOD/");
+  //SH::ScanDir().filePattern("user.moles.8671065.AOD._000411.pool.root").scan(sh,inputFilePath); //One indiviudual file
+
+  //Zmumu sample
+  const char* inputFilePath = gSystem->ExpandPathName ("$DataFiles/Zmumu/mc15_13TeV.361107.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zmumu.merge.DAOD_JETM3.e3601_s2576_s2132_r7725_r7676_p2666_tid08619356_00/");
+  SH::ScanDir().filePattern("DAOD_JETM3.08619356._000027.pool.root.1").scan(sh,inputFilePath); //One indiviudual file
+
+  
   //const char* inputFilePath = gSystem->ExpandPathName ("/afs/cern.ch/user/z/zhangr/work/eflowRec/r19.05-53/Run");
   //SH::ScanDir().filePattern("AOD.pool.root").scan(sh,inputFilePath); //One indiviudual file
 
@@ -49,11 +56,19 @@ int main( int argc, char* argv[] ) {
   job.sampleHandler( sh );
   job.options()->setDouble (EL::Job::optSkipEvents, 0);
   job.options()->setDouble (EL::Job::optMaxEvents, 100);
+  
+  // Chose here your config options:
+  bool SinglePionLowPerformanceStudies = false;
+  bool DijetLowPerformance = false;
+  bool DijetSubtraction = false;
+  bool Zmumu = true;
+  std::string matchScheme = (std::string)"_EM2";
+  bool UseNarrowPtRange = true;
+  bool UseNarrowEtaRange = true;
+  bool PrintDebug = false; // Printing message criteria in PFlowAna 
 
-  // Add our analysis to the job:
-  // SinglePionLowPerformanceStudies, DijetLowPerformance, DijetSubtraction, Zmumu
-  xAODPFlowAna* alg = new xAODPFlowAna(false, false, true, false);
-  //xAODPFlowAna* alg = new xAODPFlowAna(true, false, false, false);
+  
+  xAODPFlowAna* alg = new xAODPFlowAna(SinglePionLowPerformanceStudies, DijetLowPerformance, DijetSubtraction, Zmumu, matchScheme, UseNarrowPtRange, UseNarrowEtaRange, PrintDebug);
   job.algsAdd( alg );
 
   // Run the job using the local/direct driver:
